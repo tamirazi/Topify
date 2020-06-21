db38bb2a56a293c5501369162c0f813788454258
tamir azizi <tamir.azizi@gmail.com>
2020-06-22 00:27:19 +0300

    fixing more desktop styling

--- src/app/home/dashboard/menu/menu.component.ts
+++ src/app/home/dashboard/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService } from 'src/app/services/auth.service';
@@ -9,31 +9,21 @@ import { trigger, transition, style, animate } from '@angular/animations';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
   animations: [
-    trigger(
-      'inOutAnimation', 
-      [
-        transition(
-          ':enter', 
-          [
-            style({ opacity: 0 }),
-            animate('.3s ease-out', 
-                    style({opacity: 1 }))
-          ]
-        ),
-        transition(
-          ':leave', 
-          [
-            style({opacity: 1 }),
-            animate('.3s ease-in', 
-                    style({opacity: 0 }))
-          ]
-        )
-      ]
-    )
-  ]
+    trigger('inOutAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('.3s ease-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('.3s ease-in', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class MenuComponent implements OnInit {
   showMenu = false;
+  event: any;
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
@@ -49,5 +39,18 @@ export class MenuComponent implements OnInit {
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
+  }
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    // deferredPrompt = e;
+    this.event = e;
+  }
+
+  install() {
+    this.event();
   }
 }
