548f7717b06f36aa596aeb524fd102e226ed55de
tamir azizi <tamir.azizi@gmail.com>
2020-06-27 16:14:09 +0300

    animation on changes to index bg

--- src/app/home/dashboard/index-bg/index-bg.component.ts
+++ src/app/home/dashboard/index-bg/index-bg.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-index-bg',
   templateUrl: './index-bg.component.html',
   styleUrls: ['./index-bg.component.scss'],
 })
-export class IndexBgComponent implements OnInit {
+export class IndexBgComponent implements OnChanges {
   @Input() index: number;
+  pulsing = false;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnChanges(changes): void {
+    this.pulsing = true;
+  }
 }
